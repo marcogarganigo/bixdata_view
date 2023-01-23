e52b0acdf5586ba80ff7c67adaa5344b5dc6aa03
Marco Garganigo <marco.garganigo@swissbix.ch>
2023-01-23 11:59:16 +0100

    impostazione struttura progetto

--- bixdata_view/bixdata_view/settings.py
+++ bixdata_view/bixdata_view/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zsfw0)i5q4qp%cap2!xy=ea+$ghtj%aglgz)r79d-senz-d1z8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,17 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'bixdata_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
 
 ROOT_URLCONF = 'bixdata_view.urls'
 
