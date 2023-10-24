e58d339c4be8e0c5a63107996c458d38e17efcf3
marcogarganigo <marcogarga11@gmail.com>
2023-10-24 10:23:24 +0200

    -

--- bixdata_view/bixdata_app/views/script_view.py
+++ bixdata_view/bixdata_app/views/script_view.py
@@ -10,9 +10,12 @@ def script_test(request):
     return HttpResponse('test')
     
 def script_call(request,function):
-    bl=ScriptBusinessLogic()
-    result=bl.update_deals()
+    if function in globals() and callable(globals()[function]):
+        # Call the function using globals()
+        func = globals()[function]
+        func()
     
 def update_deals():
     bl=ScriptBusinessLogic()
     result=bl.update_deals()
+    return HttpResponse(result)
