b73442f8569b06597ae3bb06f70a22291485760d
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-10-26 09:04:34 +0200

    .

--- bixdata_view/bixdata_app/views/businesslogic/script_logic.py
+++ bixdata_view/bixdata_app/views/businesslogic/script_logic.py
@@ -37,39 +37,42 @@ class ScriptLogic:
     
     def update_deals(self):
         log=''
-        return_value=False
         deal_table_obj=Table('deal')
         dealline_table_obj=Table('dealline')
-        adiuto_db_helper_obj=DatabaseHelper('adiuto')
-        bixdata_db_helper_obj=DatabaseHelper('default')
+        adiuto_db_obj=DatabaseHelper('adiuto')
+        bixdata_db_obj=DatabaseHelper('default')
         
         conditions=list()
         conditions.append("recordid_='00000000000000000000000000001375'")
         deal_records= deal_table_obj.get_records(conditions)
-        for deal_record in deal_records:
-            log+=f"Deal: {deal_record['dealname']}"
-            recordid_deal=deal_record['recordid_']
-            deal_record_obj=Record('deal',deal_record['recordid_'])
-            adi_deal_dict = adiuto_db_helper_obj.sql_query_row(f"SELECT * FROM VA1028 WHERE F1052='{recordid_deal}' AND FENA=-1")
+        for deal_record_dict in deal_records:
+            log+=f"Deal: {deal_record_dict['dealname']}"
+            recordid_deal=deal_record_dict['recordid_']
+            hubspot_dealuser=deal_record_dict['dealuser']
+            deal_record_obj=Record('deal',deal_record_dict['recordid_'])
+            
+            
+            
+            dealuser_dict= bixdata_db_obj.sql_query_row(f"SELECT * FROM sys_user WHERE hubspot_dealuser='{hubspot_dealuser}'")
+            if dealuser_dict:
+                deal_record_obj.set_field('dealuser1',dealuser_dict['id'])
+                deal_record_obj.set_field('adiuto_dealuser',dealuser_dict['adiutoid'])
+            
+            adi_deal_dict = adiuto_db_obj.sql_query_row(f"SELECT * FROM VA1028 WHERE F1052='{recordid_deal}' AND FENA=-1")
             deal_record_obj.set_field('dealstage',adi_deal_dict['F1033'])
             deal_record_obj.set_field('adiuto_tech',adi_deal_dict['F1067'])
+            dealtech_dict= bixdata_db_obj.sql_query_row(f"SELECT * FROM sys_user WHERE hubspot_dealuser='{hubspot_dealuser}'")
+            if dealtech_dict:
+                deal_record_obj.set_field('project_assignedto',dealtech_dict['id'])
             
-            hubspot_dealuser=deal_record['dealuser']
-            deal_type=deal_record['type']
-            hubspot_id=deal_record['hubspot_id']
-            bix_dealuser_dict= bixdata_db_helper_obj.sql_query_row(f"SELECT * FROM sys_user WHERE hubspot_dealuser='{hubspot_dealuser}'")
-            if bix_dealuser_dict:
-                deal_record_obj.set_field('dealuser1',bix_dealuser_dict['id'])
-                deal_record_obj.set_field('adiuto_dealuser',bix_dealuser_dict['adiutoid'])
             
-            deal_record_obj.save()
             
             #Aggiornamento dettagli deal e calcolo totali    
             dealline_records=dealline_table_obj.get_records_by_linked('deal',recordid_deal)
             total_expectedcost=0
             for dealline_record in dealline_records:
                 dealline_recordid=dealline_record['recordid_']
-                adiuto_dealline = adiuto_db_helper_obj.sql_query_row(f"SELECT * FROM A1029 WHERE F1062='{recordid_deal}'")
+                adiuto_dealline = adiuto_db_obj.sql_query_row(f"SELECT * FROM A1029 WHERE F1062='{recordid_deal}'")
                 if adiuto_dealline:
                     bix_dealline=Record('dealline',dealline_recordid)
                     bix_dealline.fields['uniteffectivecost']=adiuto_dealline['F1043']
@@ -77,6 +80,9 @@ class ScriptLogic:
                         
                         
                 total_expectedcost=total_expectedcost+dealline_record['expectedcost']
+            
+            
+            deal_record_obj.save()
                 
         return log
    
