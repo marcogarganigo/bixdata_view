b7b675d8753fa954025595c2d3dc6c0ffa0072b4
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-04-04 13:39:55 +0200

    .

--- bixdata_view/bixdata_app/views/alpha.py
+++ bixdata_view/bixdata_app/views/alpha.py
@@ -1243,11 +1243,12 @@ def custom_save_record(request,tableid,recordid):
         if invoicestatus=='To Process':
             productivity='Ricavo diretto'
             hourprice=140
-            travelprice=70
+            travelprice=None
             timesheet_record.fields['print_travel']='Da fatturare'
 
             if not isempty(company_record.fields['ictpbx_price']):
                 hourprice=company_record.fields['ictpbx_price']
+                travelprice=company_record.fields['travel_price']
             timesheet_record.fields['print_hourprice']='Fr.'+ str(hourprice)+'.--'
 
             if not isempty(project_record.recordid):
@@ -1257,7 +1258,13 @@ def custom_save_record(request,tableid,recordid):
             if not isempty(ticket_record.recordid):
                 if  ticket_record.fields['vtestatus'] != 'Closed':
                     invoicestatus='To invoice when Ticket Closed'
-
+            timesheet_record.fields['hourprice']=hourprice
+            timesheet_record.fields['workprice']=hourprice*worktime_decimal;
+            if travelprice:
+                timesheet_record.fields['travelprice']=travelprice;
+            else:
+                timesheet_record.fields['travelprice']=hourprice*travel_time_decimal;
+            timesheet_record.fields['totalprice']=timesheet_record.fields['workprice']+timesheet_record.fields['travelprice']
             if invoicestatus=='To Process':
                 invoicestatus='To Invoice'
 
