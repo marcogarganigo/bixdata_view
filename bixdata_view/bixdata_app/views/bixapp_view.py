03e785705eeced97d7ad39c20dfb1892949d4e3c
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-04-26 11:47:11 +0200

    .

--- bixdata_view/bixdata_app/views/bixapp_view.py
+++ bixdata_view/bixdata_app/views/bixapp_view.py
@@ -160,8 +160,8 @@ def get_content_records(request):
            context_records_filters['filter_fields'][filter_field_key]=filter_field
     
     
-    hv.context['block_search_fields'] = ''
-    return hv.render_template('content/records_app.html')
+    hv.context['block_search_fields'] = render_to_string('block/records/records_filters.html',context_records_filters, request)
+    return hv.render_template('content/records.html')
 
 
 @login_required(login_url='/login/')
