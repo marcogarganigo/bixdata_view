32e07b18d4e1f206ab69c2511e933fdfa1f58cf7
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-04-29 10:31:45 +0200

    .

--- bixdata_view/bixdata_app/views/bixapp_view.py
+++ bixdata_view/bixdata_app/views/bixapp_view.py
@@ -152,15 +152,7 @@ def get_content_records(request):
     t=Table(tableid=tableid,userid=1)
     t.context='search_fields'
     filter_fields=t.get_fields()
-    context_records_filters=dict()
-    context_records_filters['filter_fields']=dict()
-    
-    for filter_fields_label_key,filter_fields_label in filter_fields.items():
-        for filter_field_key,filter_field in filter_fields_label.items():
-           context_records_filters['filter_fields'][filter_field_key]=filter_field
-    
-    
-    hv.context['block_search_fields'] = render_to_string('block/records/records_filters.html',context_records_filters, request)
+  
     return hv.render_template('content/records_app.html')
 
 
@@ -303,4 +295,146 @@ def get_records_table(request, tableid, master_tableid='', master_recordid='', s
         records[records_index] = record
 
     records_table = bix_render_to_string('block/records/records_table_app.html', context, request)
+    return records_table
+
+
+@login_required(login_url='/login/')
+def get_records_table_render(request):
+    tableid = request.POST.get('tableid')
+    master_tableid = request.POST.get('master_tableid')
+    master_recordid = request.POST.get('master_recordid')
+    searchTerm = request.POST.get('searchTerm')
+    viewid = request.POST.get('viewid')
+    filters=request.POST.get('filters')
+    filters_dict=dict()
+    order_field = request.POST.get('order_field')
+    order = request.POST.get('order')
+    currentpage = request.POST.get('currentpage')
+    if (currentpage == ''):
+        currentpage = 1
+    render = get_records_table(request, tableid, master_tableid, master_recordid, searchTerm, viewid, currentpage,order_field, order, filters)
+    return HttpResponse(render)
+
+
+@login_required(login_url='/login/')
+def get_records_table(request, tableid, master_tableid='', master_recordid='', searchTerm='', viewid='', currentpage=1,order_field='', order='', filters=dict()):
+    userid = get_userid(request.user.id)
+    table_type = 'standard'
+    table_height = '100%'
+
+    if master_tableid == '':
+        master_tableid = None
+
+    if master_recordid == '':
+        master_recordid = None
+
+    if master_tableid:
+        table_height = '500px'
+        table_type = 'linked'
+
+    post = {
+        'tableid': tableid,
+        'searchTerm': searchTerm,
+        'viewid': viewid,
+        'currentpage': currentpage,
+        'order_field': order_field,
+        'order': order,
+        'master_tableid': master_tableid,
+        'master_recordid': master_recordid,
+        'userid': userid,
+        'filters':filters
+    }
+    response = requests.post(f"{bixdata_server}bixdata/index.php/rest_controller/get_records", data=post)
+    response_dict = json.loads(response.text)
+    columns = response_dict['columns']
+    records = response_dict['records']
+    reports = response_dict['reports']
+    other_values = dict()
+
+    if order_field == '' or order_field == None:
+        order_field = columns[3]['desc']
+        order = 'asc'
+
+    if order == 'asc':
+        icon = 'mdi mdi-arrow-down-thin'
+    else:
+        icon = 'mdi mdi-arrow-up-thin'
+
+    context = {
+        'records': records,
+        'reports': reports,
+        'columns': columns,
+        'tableid': tableid,
+        'table_height': table_height,
+        'table_type': table_type,
+        'other_values': other_values,
+        'currentpage': currentpage,
+        'order_field': order_field,
+        'order': order,
+        'icon': icon
+    }
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f"SELECT column_width FROM sys_user_column_width WHERE tableid = %s AND userid = %s",
+            [tableid, get_userid(request.user.id)]
+        )
+        column_width = cursor.fetchone()
+
+        if column_width:
+            column_width = column_width[0]
+            context['column_width'] = column_width
+
+    for records_index, record in enumerate(records):
+        for record_index, value in enumerate(record):
+            record[record_index] = dict()
+            record[record_index]['value'] = value
+            record[record_index]['code'] = value
+            record[record_index]['fieldtype'] = columns[record_index]['fieldtypeid'];
+            record[record_index]['fieldtypeweb'] = columns[record_index]['fieldtypewebid'];
+            if record[record_index]['fieldtype'] == 'linked':
+                if isinstance(value, str):
+                    value = value.split('|:|')
+                    if (len(value) > 2):
+                        record[record_index]['value'] = value[0]
+                        record[record_index]['link_recordid'] = value[1]
+                        record[record_index]['link_tableid'] = value[2]
+                        record[record_index]['fieldtype'] = 'linked'
+            if record[record_index]['fieldtype'] == 'Utente':
+                code = record[record_index]['code']
+                code = code.replace(' ', '.')
+                code = code.lower()
+                record[record_index]['code'] = code
+            if record[record_index]['fieldtype'] == 'Numero':
+                num_str = record[record_index]['value']
+
+                if num_str:
+                    # Split the number into the integer and decimal parts
+                    if '.' in num_str:
+                        integer_part, decimal_part = num_str.split('.')
+                    else:
+                        integer_part, decimal_part = num_str, ""
+
+                    if decimal_part == '':
+                        decimal_part = '00'
+
+                    # Add thousands separators (period) to the integer part
+                    integer_part = format(int(integer_part), ',').replace(',', '.')
+                    formatted_num = f"{integer_part},{decimal_part}"
+
+                    record[record_index]['value'] = formatted_num
+
+            if record[record_index]['value'] == 'Validazionetecnica':
+                record[record_index]['fieldtype'] = 'lookup'
+                record[record_index]['fieldbackground'] = 'green'
+            if record[record_index]['value'] == 'Controllosolvibilita':
+                record[record_index]['fieldtype'] = 'lookup'
+                record[record_index]['fieldbackground'] = 'red'
+            if record[record_index]['value'] == 'Chiusovinto':
+                record[record_index]['fieldtype'] = 'lookup'
+                record[record_index]['fieldbackground'] = 'blue'
+
+        records[records_index] = record
+
+    records_table = bix_render_to_string('block/records/records_table.html', context, request)
     return records_table
