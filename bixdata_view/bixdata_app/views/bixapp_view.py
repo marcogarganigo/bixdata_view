ae3dc48da04e7a1f03eaa35bdabed7bc8c90a8d9
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-05-02 10:52:35 +0200

    .

--- bixdata_view/bixdata_app/views/bixapp_view.py
+++ bixdata_view/bixdata_app/views/bixapp_view.py
@@ -296,3 +296,113 @@ def get_records_table(request, tableid, master_tableid='', master_recordid='', s
 
     records_table = bix_render_to_string('block/records/records_table_app.html', context, request)
     return records_table
+
+
+@login_required(login_url='/login/')
+def get_record_card(request):
+    tableid = request.POST.get('tableid')
+    recordid = request.POST.get('recordid')
+    master_tableid = request.POST.get('master_tableid')
+    master_recordid = request.POST.get('master_recordid')
+    userid = request.user.id
+    return HttpResponse(get_block_record_card(request, tableid, recordid, userid, master_tableid, master_recordid))
+
+
+# Questa funzione serve per creare la record card e ritorna la card come stringa
+def get_block_record_card(request, tableid, recordid, userid, master_tableid='', master_recordid=''):
+    context = dict()
+    context['block_record_badge'] = get_block_record_badge(tableid, recordid)
+    context['block_record_linked'] = get_block_record_linked(tableid, recordid)
+    context['block_record_fields'] = ""
+    context['recordid'] = recordid
+    context['tableid'] = tableid
+    context['master_tableid']=master_tableid
+    context['master_recordid']=master_recordid
+    context['layout_setting'] = get_user_setting(request, 'record_open_layout')
+    if tableid == 'ticket':
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT freshdeskid FROM user_ticket WHERE recordid_={recordid}")
+            freshdeskid = cursor.fetchone()[0]
+            context['freshdeskid'] = freshdeskid
+    if tableid == 'deal':
+        deal_record=Record(tableid='deal',recordid=recordid)
+        context['dealstatus']=deal_record.fields['dealstatus']
+    context['userid'] = userid
+    context['user_table_settings'] = get_user_table_settings(userid, tableid)
+    # TODO: recuperare i dati dal table settings generico
+    context['recordtab']='fields'
+    if tableid == 'deal':
+        context['recordtab']='linked'
+    # returned = user_agent(request, 'block/record/record_card.html', 'block/record/record_card_mobile.html', context)
+    return render_to_string('block/record/record_card.html', context)
+
+
+# Questa funzione serve per creare il badge
+def get_block_record_badge(tableid, recordid):
+    print(f'get_block_record_badge {tableid} {recordid}')
+    context = dict()
+
+    post = {
+        'tableid': tableid,
+        'recordid': recordid,
+    }
+    # response = requests.post("http://10.0.0.133:8822/bixdata/index.php/rest_controller/get_fissi", data=post)
+    # response_dict = json.loads(response.text)
+    sql = f"SELECT sys_field.* FROM sys_field join sys_user_order on sys_field.fieldid=sys_user_order.fieldid WHERE sys_user_order.userid=1 AND sys_user_order.tableid='{tableid}' AND typePreference='campiFissi' ORDER BY fieldorder asc"
+
+    fields = db_query_sql(sql)
+    values = db_get_row(f"user_{tableid}", "*", f"recordid_='{recordid}'")
+    context_fields = dict()
+    for field in fields:
+        fieldid = field['fieldid']
+        field['value'] = values[fieldid]
+        context_fields[fieldid] = field
+
+    context['fields'] = context_fields
+
+    records_table = ""
+    block_record_badge = ''
+
+    if tableid == 'company':
+        sql = f"SELECT DISTINCT type FROM user_servicecontract WHERE recordidcompany_='{recordid}' AND STATUS='In Progress'"
+        context_fields['services'] = db_query_sql(sql)
+        context['fields'] = context_fields
+        block_record_badge = render_to_string('block/record/custom/record_badge_company.html', context)
+    elif tableid == 'deal':
+        deal_record = Record('deal', recordid)
+        company_record = Record(tableid='company', recordid=deal_record.fields['recordidcompany_'])
+        deal_record.fields['companyname'] = company_record.fields['companyname']
+        context['fields'] = deal_record.fields
+        block_record_badge = render_to_string('block/record/custom/record_badge_deal.html', context)
+    else:
+        block_record_badge = render_to_string('block/record/record_badge.html', context)
+    return block_record_badge
+
+
+def get_block_record_linked(tableid, recordid):
+    context = dict()
+
+    rows = db_query_sql(
+        f"SELECT sys_table_link.*,typepreference,sys_user_order.ID FROM sys_table_link JOIN sys_user_order ON sys_table_link.tableid=sys_user_order.tableid AND sys_table_link.tablelinkid=sys_user_order.fieldid  WHERE sys_table_link.tableid = '{tableid}' AND typepreference='keylabel'")
+
+    linked_tables = list()
+    for row in rows:
+        linked_table = dict()
+        linked_tableid = row['tablelinkid']
+        table_name = 'test'
+        linked_table['table_name'] = db_get_value("sys_table", "description", f"id='{linked_tableid}'")
+        table_count = db_get_count(f"user_{linked_tableid}", f"recordid{tableid}_='{recordid}'")
+        linked_table['table_count'] = table_count
+        linked_table['tableid'] = linked_tableid
+        linked_tables.append(linked_table)
+
+    context['linked_tables'] = linked_tables
+    context['tableid'] = tableid
+    context['recordid'] = recordid
+
+    record_linked_labels = render_to_string('block/record/record_linked.html', context)
+    return record_linked_labels
+
+
+
+
