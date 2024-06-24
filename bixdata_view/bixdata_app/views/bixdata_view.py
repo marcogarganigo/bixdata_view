013164293cfc0777bba0b5cf2811238f7f5799cf
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-06-24 15:10:57 +0200

    .

--- bixdata_view/bixdata_app/views/bixdata_view.py
+++ bixdata_view/bixdata_app/views/bixdata_view.py
@@ -154,8 +154,9 @@ def get_content_records(request):
     context_records_filters['filter_fields'] = dict()
 
     for filter_fields_label_key, filter_fields_label in filter_fields.items():
-        for filter_field_key, filter_field in filter_fields_label.items():
-            context_records_filters['filter_fields'][filter_field_key] = filter_field
+        if filter_fields_label:
+            for filter_field_key, filter_field in filter_fields_label.items():
+                context_records_filters['filter_fields'][filter_field_key] = filter_field
 
     hv.context['block_search_fields'] = render_to_string('block/records/records_filters.html', context_records_filters,
                                                          request)
